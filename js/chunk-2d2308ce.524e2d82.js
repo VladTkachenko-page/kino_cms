278a42ba54f79aef80c01a33692b3eab90b68124
VladTkachenko-page <63444330+VladTkachenko-page@users.noreply.github.com>
2021-11-11 13:42:27 +0200

    Add files via upload

---
+++ js/chunk-2d2308ce.524e2d82.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2308ce"],{ed81:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v(" 8 ")])},u=[],l=t("2877"),r={},s=Object(l["a"])(r,c,u,!1,null,null,null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d2308ce.524e2d82.js.map
---
+++ js/chunk-2d237ec0.650e91d3.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d237ec0"],{fcd1:function(n,e,c){"use strict";c.r(e);var t=function(){var n=this,e=n.$createElement,c=n._self._c||e;return c("div",[n._v(" 1 ")])},u=[],l=c("2877"),r={},s=Object(l["a"])(r,t,u,!1,null,null,null);e["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d237ec0.650e91d3.js.map
